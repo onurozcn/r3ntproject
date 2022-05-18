34c18ab3fb661ae2d59432ee9de53f0bb5459bd2
Onur Ozcan <ozcan-onur@hotmail.com>
2022-05-18 22:03:45 +0200

    initial commit

---
+++ frontend/src/components/product-card.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'ProductCard',
+  props: ['product'],
+}
+</script>
+
+<template lang="pug">
+.box
+  h2 {{product.name}} {{product.class}}
+   
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid black;
+  border-radius: 0.3rem;
+}
+</style>
---
+++ frontend/src/views/product.vue
@@ -0,0 +1,29 @@
+<script>
+// @ is an alias to /src
+/* eslint-disable */
+import axios from 'axios'
+import ProductCard from '@/components/product-card.vue'
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Product',
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async created() {
+    const productsRequest = await axios.get('/api/products')
+    this.products = productsRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Rent Project 
+    h2 PRODUCTS
+    product-card(v-for="product in products" :product="product")
+</template>
