a997a31e942474bbe78c9af0563821d6c53b52ac
Onur Ozcan <ozcan-onur@hotmail.com>
2022-08-19 15:49:05 +0200

    Make changes according to Product Model

--- frontend/src/views/company-products.vue
+++ frontend/src/views/company-products.vue
@@ -1,5 +1,4 @@
 <script>
-// import OrderCard from '@/components/order-card.vue'
 import { mapActions, mapState } from 'vuex'
 
 export default {
@@ -14,14 +13,11 @@ export default {
     ...mapState([ 'user']),
   },
   async created() {
-    // this.user = await this.fetchSession()
-    console.log("CREATED : ",this.user._id)
     this.companyProducts = await this.fetchCompanyProducts(this.user._id)
   },
   methods: {
     ...mapActions(['fetchCompanyProducts', 'deleteProduct']),
     async deleteProd(id) {
-      console.log(id)
       await this.deleteProduct(id)
       location.reload()
       // this.$router.push(`/`).catch(() => {})
@@ -36,12 +32,11 @@ export default {
       .col-9.container.card-container(v-for="product in companyProducts")
         .row
           .col-6 
-            img(src='https://th.bing.com/th/id/R.481249181a9952b4f26e84ac8ca731d5?rik=LADSC%2buUfrk8bQ&riu=http%3a%2f%2feskipaper.com%2fimages%2fbatmobile-1.jpg&ehk=PVKsAAGBELoHEOa4190yuzhh02F%2fMXEvg6pG%2bV1GS18%3d&risl=&pid=ImgRaw&r=0' width='100%' height='100%')
+            img(:src='`${product.photo}`' width='100%' height='100%')
           .col-6
             h3 {{ product.name }}
             h5 Product Price: 
               p {{ product.price }} €
-            //- button.btn.btn-primary(href=`/edit-products/${product._id}`) Edit Product
             button.btn
              router-link(:to="`/edit-products/${product._id}`") Edit Product
             button.btn.btn-primary(@click='deleteProd(product._id)') Delete Product
