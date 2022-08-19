435cb9161a311932efbedd4c5cf842229e5ed142
Onur Ozcan <ozcan-onur@hotmail.com>
2022-08-19 15:43:27 +0200

    Make changes according to Product Model

--- frontend/src/views/edit-product.vue
+++ frontend/src/views/edit-product.vue
@@ -7,14 +7,14 @@ export default {
   data() {
     return {
       name: '',
-      type: '',
-      classy: '',
+      // type: '',
+      // classy: '',
       gear: '',
       seat: 0,
       fuel: '',
       amount: 0.0,
       price: 0.0,
-      isAvailable: false,
+      // isAvailable: false,
       pickUpPoint: '',
       product: null,
       backendError: null,
@@ -24,14 +24,14 @@ export default {
   async created() {
     const product = await this.fetchProduct(this.$route.params.id)
     this.name = product.name
-    this.type = product.type
-    this.classy = product.classy
+    // this.type = product.type
+    // this.classy = product.classy
     this.gear = product.gear
     this.amount = product.amount
     this.seat = product.seat
     this.fuel= product.fuel
     this.price = product.price
-    this.isAvailable = product.isAvailable
+    // this.isAvailable = product.isAvailable
     this.pickUpPoint = product.pickUpPoint
   },
   methods: {
@@ -42,14 +42,15 @@ export default {
       try {
         await this.editProduct({
           name: this.name,
-          type: this.type,
-          classy: this.classy,
+          // type: this.type,
+          // classy: this.classy,
           gear: this.gear,
           seat: this.seat,
           fuel: this.fuel,
           amount: this.amount,
           price: this.price,
-          isAvailable: this.isAvailable,
+          // isAvailable: this.isAvailable,
+          photo: this.photo,
           pickUpPoint: this.pickUpPoint,
           id: this.$route.params.id,
         })
@@ -68,10 +69,10 @@ export default {
       h1 Edit Product
       label(for="name") Product Name:&nbsp;
         input(v-model="name" id="name" type="text" placeholder="Product name" required)
-      label(for="type") Product Type:&nbsp;
-        input(v-model="type" id="type" type="text" placeholder="Product type" required)
-      label(for="classy") Product Class:&nbsp;
-        input(v-model="classy" id="classy" type="text" placeholder="Product class" required)
+      //- label(for="type") Product Type:&nbsp;
+      //-   input(v-model="type" id="type" type="text" placeholder="Product type" required)
+      //- label(for="classy") Product Class:&nbsp;
+      //-   input(v-model="classy" id="classy" type="text" placeholder="Product class" required)
       label(for="gear") Gear Type:&nbsp;
         input(v-model="gear" id="gear" type="text" placeholder="Gear type" required)
       label(for="seat") Number of Seats:&nbsp;
@@ -82,8 +83,10 @@ export default {
         input(v-model="amount" id="amount" type="number" placeholder="Amount" required)
       label(for="price") Price per Day:&nbsp;
         input(v-model="price" id="price" type="number" placeholder="Price per day" required)
-      label(for="isAvailable") Availability:&nbsp;
-        input(v-model="isAvailable" id="isAvailable" type="text" placeholder="Is available?" required)
+      //- label(for="isAvailable") Availability:&nbsp;
+      //-   input(v-model="isAvailable" id="isAvailable" type="text" placeholder="Is available?" required)
+      label(for="photo") Photo:&nbsp;
+        input(v-model="photo" id="photo" type="text" placeholder="Photo URL" required)
       label(for="pickUpPoint") Pick up Point:&nbsp;
         input(v-model="pickUpPoint" id="pickUpPoint" type="text" placeholder="Pick up point" required)
       input.button(type="submit" value="Edit Product")
