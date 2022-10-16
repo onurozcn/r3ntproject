97984e1247644b2cbec438a7ec9f80995b6c9fb2
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-16 16:18:48 +0200

    Add nodemailer. Send e-mail after register

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -1,21 +1,39 @@
 const express = require('express')
 const passport = require('passport')
 
+const router = express.Router()
+
+const nodemailer = require('nodemailer')
+const sendgridTransport = require('nodemailer-sendgrid-transport')
+
 const User = require('../models/user')
 
-const router = express.Router()
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: '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',
+    },
+  })
+)
 
 router.get('/session', (req, res) => {
   res.send(req.user)
 })
 
 router.post('/', async (req, res, next) => {
-  const { name, age, email, password, isCompany } = req.body
+  const { name, email, password, isCompany } = req.body
 
   try {
-    const user = await User.register({ name, age, email, isCompany }, password)
+    const user = await User.register({ name, email, isCompany }, password)
     res.send(user)
+    transporter.sendMail({
+      to: email,
+      from: 'onurozcn182@gmail.com',
+      subject: 'Sign-up succeeded to R3ntals',
+      html: `<h1> Thank you ${name}, you are successfully registered</h1>`,
+    })
   } catch (e) {
+    // res.send(user)
     next(e)
   }
 })
