4fd9ca113c2786c1f6448816af4bf33f024829ef
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-16 16:16:56 +0200

    Refactor product model

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -7,23 +7,9 @@ const productSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  // type: {
-  //   type: String,
-  //   required: true,
-  //   trim: true,
-  // },
-  // classy: {
-  //   type: String,
-  //   required: true,
-  //   trim: true,
-  // },
+
   gear: {
     type: String,
-    required: true,
-    trim: true,
-  },
-  seat: {
-    type: Number,
     required: true,
     trim: true,
   },
@@ -45,31 +31,24 @@ const productSchema = new mongoose.Schema({
     type: Number,
     trim: true,
   },
-  // isAvailable: Boolean,
   pickUpPoint: {
     type: String,
     trim: true,
-  }, // for now it takes string, later on it should take a coordinate or location directly
+  },
   photo: {
     type: String,
     trim: true,
     required: true,
   },
-  revs: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Review',
-    autopopulate: true,
-  }]
+  revs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review',
+      autopopulate: true,
+    },
+  ],
 })
 
-class Product {
-  async addRev(rev) {
-    this.revs.push(rev)
-    await this.save()
-  }
-}
-
-productSchema.loadClass(Product)
 productSchema.plugin(autopopulate)
 
 module.exports = mongoose.model('Product', productSchema)
