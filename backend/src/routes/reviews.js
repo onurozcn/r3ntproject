a6c835bcb6ffc19cf54a33051a70a8793b526231
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-18 11:24:52 +0200

    apply eslint rule

--- backend/src/routes/reviews.js
+++ backend/src/routes/reviews.js
@@ -1,18 +1,18 @@
 const express = require('express')
+
 const router = express.Router()
 
 const shopsController = require('../controllers/shopController')
 
-
 router.get('/', shopsController.getAllReviews)
 
-router.get('/product/:product_id', shopsController.getProductReview )
+router.get('/product/:product_id', shopsController.getProductReview)
 
 router.get('/user/:user_id/', shopsController.getReviewByUserId)
 
 router.post('/', shopsController.createAReview)
 
-router.patch('/update-review/:rev_id', shopsController.updateProductReview )
+router.patch('/update-review/:rev_id', shopsController.updateProductReview)
 
 router.delete('/', shopsController.deleteAllReviews)
 
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express')
+
 const router = express.Router()
 
-const usersController = require('../controllers/userController');
-
+const usersController = require('../controllers/userController')
 
 router.get('/', usersController.getAllUsers)
 
