6f15049e2fb922eaeb0dc82bfa8ad64e2551988d
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-24 07:09:11 +0200

    Move logic back to routes file

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -1,16 +1,73 @@
 const express = require('express')
+const Order = require('../models/order')
+
 const router = express.Router()
 
-const usersController = require('../controllers/userController')
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await Order.find({})
+    res.send(orders)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.get('/', usersController.getAllOrders)
+router.get('/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id)
+    if (!order) {
+      res.sendStatus(404)
+      return
+    }
+    res.send(order)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.get('/:id', usersController.getOrderById)
+router.get('/user/:id/', async (req, res, next) => {
+  try {
+    const orders = await Order.find({ user: req.params.id })
+    if (!orders) {
+      res.sendStatus(404)
+      return
+    }
+    res.send(orders)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.get('/user/:id/', usersController.getUserOrdersByUserId)
+router.post('/', async (req, res, next) => {
+  const { product, invoice, user } = req.body
 
-router.post('/', usersController.createOrder )
+  try {
+    if (!product || !invoice || !user) {
+      res
+        .send({
+          message: 'Missing parameters/fields.',
+        })
+        .status(400)
+      return
+    }
+    const order = await Order.create({
+      product,
+      invoice,
+      user,
+    })
+    res.send(order)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.delete('/', usersController.deleteAllOrders)
+router.delete('/', async (req, res, next) => {
+  try {
+    await Order.deleteMany()
+    res.sendStatus(200)
+  } catch (e) {
+    next(e)
+  }
+})
 
 module.exports = router
