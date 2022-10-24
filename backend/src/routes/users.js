bc4fc659596c2d35d44090633c08d630f7db2584
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-24 07:09:54 +0200

    Move logic back to routes file

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,18 +2,78 @@ const express = require('express')
 
 const router = express.Router()
 
-const usersController = require('../controllers/userController')
+const User = require('../models/user')
 
-router.get('/', usersController.getAllUsers)
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.get('/:id/json', usersController.getUserById)
+router.get('/:id/json', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      res.sendStatus(404)
+      return
+    }
+    res.send(user)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.patch('/:id', usersController.updateUserById)
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      email: req.body.email,
+      isCompany: req.body.isCompany,
+    })
+    res.send(user)
+  } catch (e) {
+    next(e)
+  }
+})
 
-router.post('/', usersController.createUser)
+router.post('/', async (req, res) => {
+  const { name, email, isCompany } = req.body
 
-router.delete('/deleteUserById/:id', usersController.deleteUserById)
+  if (!email || !name || !isCompany) {
+    res
+      .send({
+        message: 'Missing fields.',
+      })
+      .status(400)
+    return
+  }
+  const user = await User.create({
+    name,
+    email,
+    isCompany,
+  })
+  res.send(user)
+})
 
-router.delete('/deleteAllUsers', usersController.deleteAllUsers)
+router.delete('/deleteUserById/:id', async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.sendStatus(200)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.delete('/deleteAllUsers', async (req, res, next) => {
+  try {
+    await User.deleteMany()
+    res.sendStatus(200)
+  } catch (e) {
+    next(e)
+  }
+})
 
 module.exports = router
