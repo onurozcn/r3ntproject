f9a3d1e8eb05c84e10668f3517362727bca4bc65
Onur Ozcan <ozcan-onur@hotmail.com>
2022-12-27 00:39:59 +0100

    Refactor code

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,15 +1,11 @@
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapState } from 'vuex'
+import FooterComponent from './components/footer.vue'
+import HeaderComponent from './components/header.vue'
 
 export default {
   name: 'App',
-  methods: {
-    ...mapActions(['logout']),
-    async doLogout() {
-      await this.logout()
-      this.$router.push('/login')
-    },
-  },
+  components: {FooterComponent, HeaderComponent},
   computed: {
     ...mapState(['user', 'products']),
   },
@@ -19,35 +15,9 @@ export default {
 <template lang="pug">
   #app
     #nav
-      nav#sticky-nav.navbar.navbar-expand-lg.bg-light
-        .container-fluid
-          a.navbar-brand(href='/') R3NTALS
-          button.navbar-toggler(type='button', data-bs-toggle='collapse', data-bs-target='#navbarNav', aria-controls='navbarNav', aria-expanded='false', aria-label='Toggle navigation')
-            span.navbar-toggler-icon
-          #navbarNav.collapse.navbar-collapse
-            ul.navbar-nav.justify-content-end
-              li.nav-item
-                router-link.nav-link(v-if="user && !user.isCompany" to="/order") Orders&Invoices
-              li.nav-item
-                router-link.nav-link(v-if="!user" to="/login") Login
-              li.nav-item
-                router-link.nav-link(v-if="!user" to="/register") Register
-              li.nav-item
-                router-link.nav-link(v-if="user && user.isCompany" to="/company-products") Products
-              li.nav-item
-                router-link.nav-link(v-if="user && user.isCompany" to="/add-product") Add Product
-              li.nav-item
-                a.nav-link(v-if="user" @click="doLogout" href="#") Logout
-    
-
-    //- ul#footer.nav.fixed-bottom.justify-content-center
-    //-   li.nav-item
-    //-     a.nav-link(aria-current='page', href='#') Privacy Policy
-    //-   li.nav-item
-    //-     a.nav-link(href='#') About Us
-    //-   li.nav-item
-    //-     a.nav-link(href='#') Terms of Use
+      HeaderComponent
     router-view
+    FooterComponent
   
  </template>
 
@@ -59,6 +29,7 @@ export default {
   color: #01070d;
   background-color: #e6eaee;
   max-width: 100%;
+  min-width: 300px;
   overflow-x: hidden;
 }
 
@@ -75,10 +46,5 @@ export default {
       color: blue;
     }
   }
-}
-#footer {
-  background-color: #eff3f6;
-  
-
 }
 </style>
